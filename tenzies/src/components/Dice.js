587847c69ffca799575a60d434364c141f14a4ed
Harmeet Matharoo <hmjatt@gmail.com>
2022-08-30 00:47:34 -0600

    Put *Real Dots* on the *Dice*. WIP

--- tenzies/src/components/Dice.js
+++ tenzies/src/components/Dice.js
@@ -1,7 +1,66 @@
 function Dice(props) {
+	let diceSpanEles;
+
+	if(props.value === 1){
+		diceSpanEles =  <div className="dice first-face">
+							<span className="dot"> </span>
+						</div>;
+	} else if(props.value === 2) {
+		diceSpanEles =  <div className="dice second-face">
+							<span className="dot"> </span>
+							<span className="dot"> </span>
+						</div>;
+	} else if(props.value === 3) {
+		diceSpanEles =  <div className="dice third-face">
+							<span className="dot"></span>
+							<span className="dot"></span>
+							<span className="dot"></span>
+						</div>;		
+	} else if(props.value === 4) {
+		diceSpanEles =  <div className="fourth-face dice">
+							<div className="column">
+								<span className="dot"></span>
+								<span className="dot"></span>
+							</div>
+							<div className="column">
+								<span className="dot"></span>
+								<span className="dot"></span>
+							</div>
+						</div>		
+	} else if(props.value === 5) {
+		diceSpanEles =  <div className="fifth-face dice">
+							<div className="column">
+								<span className="dot"></span>
+								<span className="dot"></span>
+							</div>
+							
+							<div className="column">
+								<span className="dot"></span>
+							</div>
+							
+							<div className="column">
+								<span className="dot"></span>
+								<span className="dot"></span>
+							</div>
+						</div>		
+	} else if(props.value === 6) {
+		diceSpanEles =  <div className="fourth-face dice">
+							<div className="column">
+								<span className="dot"></span>
+								<span className="dot"></span>
+								<span className="dot"></span>
+							</div>
+							<div className="column">
+								<span className="dot"></span>
+								<span className="dot"></span>
+								<span className="dot"></span>
+							</div>
+						</div>		
+	} 
+
     return (
         <div className="dice-face">
-            <h2 className="dice-num">{props.value}</h2>
+			{diceSpanEles}
         </div>
     );
 }
